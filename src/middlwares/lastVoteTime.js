608d91f5d0b011317381fc84abd7acde1d7c3415
Stepan Boychuk <demonomiconnnn@gmail.com>
2024-03-28 11:17:42 +0200

    Uncommended lastVoteTime middlware

--- src/middlwares/lastVoteTime.js
+++ src/middlwares/lastVoteTime.js
@@ -1,14 +1,14 @@
 const Vote = require("./../models/Vote.js");
 
 const lastVoteTime = async (req, res, next) => {
-  // const oneHourAgo = new Date(Date.now() - 3600000);
-  // const vote = await Vote.findOne({
-  //   user: req.user.id,
-  //   updatedAt: { $gt: oneHourAgo },
-  // });
-  // if (vote) {
-  //   return res.status(429).send("You can vote once an hour");
-  // }
+  const oneHourAgo = new Date(Date.now() - 3600000);
+  const vote = await Vote.findOne({
+    user: req.user.id,
+    updatedAt: { $gt: oneHourAgo },
+  });
+  if (vote) {
+    return res.status(429).send("You can vote once an hour");
+  }
   next();
 };
 
